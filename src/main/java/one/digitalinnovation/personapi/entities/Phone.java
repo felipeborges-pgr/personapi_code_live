b93babbdff1a8059610b29c7ffaa56d46a0c3fd2
felipeborges-pgr <fg662031@gmail.com>
2021-11-24 00:03:35 -0300

    Developed person create operation

---
+++ src/main/java/one/digitalinnovation/personapi/entities/Phone.java
@@ -0,0 +1,30 @@
+package one.digitalinnovation.personapi.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.personapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private PhoneType type;
+
+    @Column(nullable = false, unique = true)
+    private String number;
+
+
+}
