2854269c5ccb98cfd7fa09da7593f3779687130f
felipeborges-pgr <fg662031@gmail.com>
2021-11-29 21:53:40 -0300

    Improved person creation funcionality

--- src/main/java/one/digitalinnovation/personapi/entities/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entities/Person.java
@@ -1,9 +1,6 @@
 package one.digitalinnovation.personapi.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -30,9 +27,54 @@ public class Person {
     @Column(nullable = false, unique = true)
     private String cpf;
 
+    @Getter
     private LocalDate birthDate;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     private List<Phone> phones = new ArrayList<>();
 
 }
+
+
+
+
+
+
+//package one.digitalinnovation.personapi.entities;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//import java.time.LocalDate;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Entity
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Person {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(nullable = false)
+//    private String firsName;
+//
+//    @Column(nullable = false)
+//    private String lastName;
+//
+//    @Column(nullable = false, unique = true)
+//    private String cpf;
+//
+//    private LocalDate birthDate;
+//
+//    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+//    private List<Phone> phones = new ArrayList<>();
+//
+//}
